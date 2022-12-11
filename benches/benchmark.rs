e95b1da646aa1d86fa68eea80e022f92305b6fb2
Peter Triller <peter.triller@1und1.de>
2022-12-11 02:13:25 +0100

    day 9 done

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,6 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use advent_of_code_2022::day6::{day6work1, day6work2};
 use advent_of_code_2022::day7::{day7work1, day7work2};
 use advent_of_code_2022::day8::{day8work1, day8work2};
+use advent_of_code_2022::day9::{day9work1, day9work2};
 
 pub fn day6_benchmark(c: &mut Criterion) {
     c.bench_function("day6a", |b| b.iter(|| day6work1()));
@@ -19,5 +20,11 @@ pub fn day8_benchmark(c: &mut Criterion) {
 }
 
 
-criterion_group!(benches,  day6_benchmark,day7_benchmark, day8_benchmark);
+pub fn day9_benchmark(c: &mut Criterion) {
+    c.bench_function("day9a", |b| b.iter(|| day9work1()));
+    c.bench_function("day9b", |b| b.iter(|| day9work2()));
+}
+
+
+criterion_group!(benches,  day6_benchmark,day7_benchmark, day8_benchmark, day9_benchmark);
 criterion_main!(benches);
--- src/day9.rs
+++ src/day9.rs
@@ -4,58 +4,77 @@ use std::io::BufRead;
 
 type Position = (i32, i32);
 
+#[derive(Debug)]
 struct RopeSpace {
     trail: HashSet<Position>,
     head: Position,
-    tail: Position,
+    tail: Vec<Position>,
 }
 
 impl RopeSpace {
-    fn new() -> Self {
+    fn new(tails: usize) -> Self {
         RopeSpace {
             trail: HashSet::new(),
             head: (0, 0),
-            tail: (0, 0),
+            tail: vec![(0, 0); tails],
         }
     }
 
     fn drag_rope<I>(&mut self, iter: &mut I)
         where
             I: Iterator<Item=Result<String, io::Error>>, {
-        self.trail.insert(self.tail.clone());
+        self.trail.insert(self.tail.last().unwrap().clone());
         for res in iter {
             let line = res.unwrap();
             let num: usize = line.get(2..).unwrap().parse().unwrap();
             let mv = match line.chars().nth(0).unwrap() {
                 'R' => (1, 0),
                 'L' => (-1, 0),
-                'D' => (0, 1),
-                'U' => (0, -1),
+                'D' => (0, -1),
+                'U' => (0, 1),
                 _ => panic!("Illegal Input")
             };
             for _ in 0..num {
                 self.head.0 += mv.0;
                 self.head.1 += mv.1;
-                self.move_tail();
-                self.trail.insert(self.tail.clone());
+                move_tail(&self.head, &mut self.tail.first_mut().unwrap());
+                for i in 0..self.tail.len() - 1 {
+                    let head = self.tail[i].clone();
+                    move_tail(&head, &mut self.tail[i+1]);
+                }
+                self.trail.insert(self.tail.last().unwrap().clone());
             }
         }
     }
-    fn move_tail(&mut self) {
-        if (self.head.1 - self.tail.1).abs() > 1 {
-            self.tail.0 = self.head.0;
-            self.tail.1 = self.head.1 + (self.head.1 - self.tail.1).signum();
-        } else if self.head.1 == self.tail.1 && (self.head.0 - self.tail.0).abs() > 1 {
-            self.tail.1 = self.head.1;
-            self.tail.0 = self.head.0 + (self.head.0 - self.tail.0).signum();
-        }
+}
+
+fn move_tail(head: &Position, tail: &mut Position) {
+    let one = (head.1 - tail.1).abs();
+    let zero = (head.0 - tail.0).abs();
+    if one > 1 && zero > 1 {
+        tail.0 = head.0 - (head.0 - tail.0).signum();
+        tail.1 = head.1 - (head.1 - tail.1).signum();
+    } else if one > 1 {
+        tail.0 = head.0;
+        tail.1 = head.1 - (head.1 - tail.1).signum();
+    } else if zero > 1 {
+        tail.1 = head.1;
+        tail.0 = head.0 - (head.0 - tail.0).signum();
     }
 }
 
 pub fn day9work1() -> io::Result<usize> {
     let file = fs::File::open(&"data/day9.txt").unwrap();
     let mut lines = io::BufReader::new(file).lines();
-    let mut rope = RopeSpace::new();
+    let mut rope = RopeSpace::new(1);
+    rope.drag_rope(&mut lines);
+    return Ok(rope.trail.len());
+}
+
+pub fn day9work2() -> io::Result<usize> {
+    let file = fs::File::open(&"data/day9.txt").unwrap();
+    let mut lines = io::BufReader::new(file).lines();
+    let mut rope = RopeSpace::new(9);
     rope.drag_rope(&mut lines);
     return Ok(rope.trail.len());
 }
