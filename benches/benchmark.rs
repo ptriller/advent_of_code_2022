3d2bc29df75ff6c74e1174618eac02676d593d17
Peter Triller <peter.triller@1und1.de>
2022-12-11 15:50:21 +0100

    day 10 done

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,8 +1,39 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use advent_of_code_2022::day1::{day1work1, day1work2};
+use advent_of_code_2022::day10::{day10work1, day10work2};
+use advent_of_code_2022::day2::{day2work1, day2work2};
+use advent_of_code_2022::day3::{day3work1, day3work2};
+use advent_of_code_2022::day4::{day4work1, day4work2};
+use advent_of_code_2022::day5::{day5work1, day5work2};
 use advent_of_code_2022::day6::{day6work1, day6work2};
 use advent_of_code_2022::day7::{day7work1, day7work2};
 use advent_of_code_2022::day8::{day8work1, day8work2};
 use advent_of_code_2022::day9::{day9work1, day9work2};
+
+pub fn day1_benchmark(c: &mut Criterion) {
+    c.bench_function("day1a", |b| b.iter(|| day1work1()));
+    c.bench_function("day1b", |b| b.iter(|| day1work2()));
+}
+
+pub fn day2_benchmark(c: &mut Criterion) {
+    c.bench_function("day2a", |b| b.iter(|| day2work1()));
+    c.bench_function("day2b", |b| b.iter(|| day2work2()));
+}
+
+pub fn day3_benchmark(c: &mut Criterion) {
+    c.bench_function("day3a", |b| b.iter(|| day3work1()));
+    c.bench_function("day3b", |b| b.iter(|| day3work2()));
+}
+
+pub fn day4_benchmark(c: &mut Criterion) {
+    c.bench_function("day4a", |b| b.iter(|| day4work1()));
+    c.bench_function("day4b", |b| b.iter(|| day4work2()));
+}
+
+pub fn day5_benchmark(c: &mut Criterion) {
+    c.bench_function("day5a", |b| b.iter(|| day5work1()));
+    c.bench_function("day5b", |b| b.iter(|| day5work2()));
+}
 
 pub fn day6_benchmark(c: &mut Criterion) {
     c.bench_function("day6a", |b| b.iter(|| day6work1()));
@@ -25,6 +56,20 @@ pub fn day9_benchmark(c: &mut Criterion) {
     c.bench_function("day9b", |b| b.iter(|| day9work2()));
 }
 
+pub fn day10_benchmark(c: &mut Criterion) {
+    c.bench_function("day10a", |b| b.iter(|| day10work1()));
+    c.bench_function("day10b", |b| b.iter(|| day10work2()));
+}
 
-criterion_group!(benches,  day6_benchmark,day7_benchmark, day8_benchmark, day9_benchmark);
+criterion_group!(benches,
+    day1_benchmark,
+    day2_benchmark,
+    day3_benchmark,
+    day4_benchmark,
+    day5_benchmark,
+    day6_benchmark,
+    day7_benchmark,
+    day8_benchmark,
+    day9_benchmark,
+    day10_benchmark);
 criterion_main!(benches);
--- src/day10.rs
+++ src/day10.rs
@@ -1,22 +1,119 @@
 use std::{fs, io};
+use std::collections::{HashSet};
 use std::io::BufRead;
+use crate::day10::Instruction::{ADDX, NOOP};
 
+#[derive(Debug)]
+enum Instruction {
+    NOOP,
+    ADDX(isize),
+}
 
-pub fn day10work1() -> io::Result<usize> {
+#[derive(Debug)]
+struct Computer {
+    instructions: Vec<Instruction>,
+    pc: usize,
+    tick: usize,
+    register: isize,
+}
+
+impl Computer {
+    fn new<I>(iter: I) -> Self
+        where
+            I: Iterator<Item=Result<String, io::Error>>, {
+        return Computer {
+            instructions: Vec::from_iter(iter.map(map_instruction)),
+            pc: 0,
+            tick: 0,
+            register: 1,
+        };
+    }
+
+    fn execute<I>(&mut self, callback: &mut I)
+        where
+            I: FnMut(usize, isize) -> bool {
+        loop {
+            match self.instructions[self.pc] {
+                NOOP => {
+                    self.tick += 1;
+                    if !callback(self.tick, self.register) { return; };
+                }
+                ADDX(val) => {
+                    self.tick += 1;
+                    if !callback(self.tick, self.register) { return; };
+                    self.tick += 1;
+                    if !callback(self.tick, self.register) { return; };
+                    self.register += val;
+                }
+            }
+            self.pc = (self.pc + 1) % self.instructions.len();
+        }
+    }
+}
+
+fn map_instruction(res: io::Result<String>) -> Instruction {
+    let line = res.unwrap();
+    if line == "noop" {
+        return NOOP;
+    } else {
+        return ADDX(line.as_str().strip_prefix("addx ").unwrap().parse().unwrap());
+    }
+}
+
+pub fn day10work1() -> io::Result<isize> {
     let file = fs::File::open(&"data/day10.txt").unwrap();
-    let lines = io::BufReader::new(file).lines();
-    return Ok(1);
+    let mut lines = io::BufReader::new(file).lines();
+    let mut computer = Computer::new(&mut lines);
+    let point = HashSet::from([20, 60, 100, 140, 180, 220]);
+    let mut sum = 0;
+    computer.execute(&mut |tick, register| -> bool {
+        if point.contains(&tick) {
+            sum += tick as isize * register;
+        }
+        tick < 220
+    });
+    return Ok(sum);
 }
 
 
+pub fn day10work2() -> io::Result<String> {
+    let file = fs::File::open(&"data/day10.txt").unwrap();
+    let mut lines = io::BufReader::new(file).lines();
+    let mut computer = Computer::new(&mut lines);
+    let mut display = vec![vec![' '; 41]; 6];
+    for row in display.iter_mut() {
+        row[40] = '\n';
+    }
+    computer.execute(&mut |tick, register| -> bool {
+        let row = (tick - 1) / 40;
+        let col = (tick - 1) % 40;
+        if (col as isize).abs_diff(register) <= 1 {
+            display[row][col] = '#';
+        } else {
+            display[row][col] = '.';
+        }
+        tick < 240
+    });
+    let screen = String::from_iter(display.iter().flat_map(|row| row.iter()));
+    return Ok(screen);
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day10::day10work1;
+    use crate::day10::{day10work1, day10work2};
 
     #[test]
     fn test_1() {
         match day10work1() {
             Ok(num) => println!("Day 10 Part 1 Signal Strength: {num}"),
+            Err(data) => panic!("Something went wrong: {}", data)
+        }
+    }
+
+    #[test]
+    fn test_2() {
+        match day10work2() {
+            Ok(num) => println!("Day 10 Part 2 Display:\n{num}"),
             Err(data) => panic!("Something went wrong: {}", data)
         }
     }
